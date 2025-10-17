03b69ee65273f264272ed4a7cd233388a131e591
ali kamali <ali70kamali1991@gmail.com>
2025-10-17 23:08:27 +0330

    creat module.css for table and creat table

--- src/Components/List.jsx
+++ src/Components/List.jsx
@@ -1,22 +1,35 @@
-import styled from "styled-components"
-
+import styled from "styled-components";
+import styles from "../Components/List.module.css"
 
 const Div = styled.div`
-    width: 70%;
-    height: 300px;
-    border: 1px solid red;
-    background-color: cornflowerblue;
-    margin: auto;
-    margin-top: 70px;
-`
+  width: 70%;
+  height: 300px;
+  border: 1px solid red;
+  background-color: cornflowerblue;
+  margin: auto;
+  margin-top: 70px;
+  border-radius: 15px;
+`;
 function List() {
   return (
-   <>
-   <Div>
-
-   </Div>
-   </>
-  )
+    <>
+      <Div>
+        <table className={styles.table}>
+          <thead>
+            <tr>
+              <th>name</th>
+              <th>family</th>
+              <th>phone number</th>
+              <th>email</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr></tr>
+          </tbody>
+        </table>
+      </Div>
+    </>
+  );
 }
 
-export default List
+export default List;
